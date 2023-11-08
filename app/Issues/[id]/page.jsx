ec1fe3a490c2da4fece5d3aa7d01f9a1b6837db7
sxs6596 <sxs6596@mavs.uta.edu>
2023-11-08 12:22:14 -0600

    added the edit issue button

--- app/Issues/[id]/page.jsx
+++ app/Issues/[id]/page.jsx
@@ -1,7 +1,9 @@
 import StatusBadge from "@/app/components/StatusBadge";
 import prisma from "@/prisma/client";
-import { Flex, Heading, Text, Card } from "@radix-ui/themes";
+import { Flex, Heading, Text, Card, Grid, Box, Button } from "@radix-ui/themes";
+import {Penclil2Icon} from "@radix-ui/react-icons";
 import { notFound } from 'next/navigation'
+import Link from "next/link";
 const IssueDetailsPage = async ({params}) => {
   const id = params.id
   const issue = await prisma.issue.findUnique({
@@ -13,6 +15,8 @@ const IssueDetailsPage = async ({params}) => {
   
   return (
     <div>
+        <Grid columns={{initial:"1", md:"2"}} gap="3">
+        <Box>
         <Heading>{issue.title}</Heading>
         <Flex gap="3" className="my-2">
         <StatusBadge status={issue.status}/>
@@ -20,6 +24,14 @@ const IssueDetailsPage = async ({params}) => {
         <Card style={{maxWidth:"400"}}>
         <Text>{issue.description}</Text>
         </Card>
+        </Box>
+        <Box>
+          <Button>
+
+            <Link href={`Issues/${issue.id}/edit`}>Edit Issues</Link>
+          </Button>
+        </Box>
+        </Grid>
 
     </div>
   )
